ff19e6daa0cb7f5cb2706392e507f135d249a6b4
Sohel Amin <sohelamincse@gmail.com>
2018-06-05 13:31:28 +0600

    :art: Logo added

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+![Logo](/logo.png)
+
 # 30 seconds of php code
 > A curated collection of useful PHP snippets that you can understand in 30 seconds or less.
 
--- scripts/build.php
+++ scripts/build.php
@@ -2,7 +2,9 @@
 
 $projectDir = dirname(__FILE__) . '/../';
 
-$readme = '# 30 seconds of php code
+$readme = '![Logo](/logo.png)
+
+# 30 seconds of php code
 > A curated collection of useful PHP snippets that you can understand in 30 seconds or less.
 
 ## Table of Contents
