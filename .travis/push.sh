0df6c4e946182d04802e44e8c608400c44ded97c
Sohel Amin <sohelamincse@gmail.com>
2018-06-09 12:24:13 +0600

    Travis changes

--- .travis/push.sh
+++ .travis/push.sh
@@ -4,7 +4,24 @@ setup_git() {
   git config --global user.name "sohelamin"
 }
 
-upload_files() {
+git_commit() {
+  if [ $TRAVIS_EVENT_TYPE != "pull_request" ]; then
+    if [ $TRAVIS_BRANCH == "master" ]; then
+      echo "Committing to master branch..."
+      git checkout master
+      git add *
+      if [ $TRAVIS_EVENT_TYPE == "cron" ]; then
+        git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [cron]"
+      elif [ $TRAVIS_EVENT_TYPE == "api" ]; then
+        git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [custom]"
+      else
+        git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+      fi
+    fi
+  fi
+}
+
+git_push() {
   if [ $TRAVIS_EVENT_TYPE != "pull_request" ]; then
     if [ $TRAVIS_BRANCH == "master" ]; then
       echo "Pushing to master branch..."
@@ -14,4 +31,5 @@ upload_files() {
 }
 
 setup_git
-upload_files
+git_commit
+git_push
